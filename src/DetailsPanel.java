cfe30e5dcde2f7625a878840118f3a8b2c8a1e07
Frederick Morrison <frederick.morrison@outlook.com>
2023-11-20 13:01:37 -0700

    Details Panel with layout declared but not used.

--- src/DetailsPanel.java
+++ src/DetailsPanel.java
@@ -3,13 +3,24 @@ import java.awt.*;
 
 public class DetailsPanel extends JPanel
 {
-    public DetailsPanel(Dimension parentSize)
+    public DetailsPanel(Dimension frameSize)
     {
         Dimension size = getPreferredSize();
-        size.width = parentSize.width / 2;
-        size.height = parentSize.height;
+        size.width = frameSize.width / 2;
+        size.height = frameSize.height;
         setPreferredSize(size);
         // put a border around our form
         setBorder(BorderFactory.createTitledBorder("Personal Details"));
+
+        JLabel nameLabel =new JLabel("Name: ");
+        JLabel occupationLabel = new JLabel("Occupation: ");
+
+        JTextField nameField = new JTextField(10); // 10 = number of columns
+        JTextField occupationField = new JTextField(10);
+
+        JButton addBtn = new JButton("Add");
+
+        setLayout(new GridBagLayout());
+        GridBagConstraints gc = new GridBagConstraints();
     }
 }
